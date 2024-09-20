202a9a3bdae0a948ea86b44eabfdd0f1d95b236b
Lohopupa <lohopupa@gmail.com>
2024-09-21 04:31:44 +0600

    Simple ui library

--- src/ui.ts
+++ src/ui.ts
@@ -1,92 +1,184 @@
 class BaseElement {
-    title: string
-    className: string
-    constructor(title: string){
+    protected title: string
+    protected className: string
+
+    constructor(title: string) {
         this.title = title
         this.className = "not-implemented"
+    }
+
+    render(parentId?: string): HTMLElement {
+        throw new Error("NOT IMPLEMENTD")
     }
 
     getId(parentId: string) {
         return (parentId == "" ? "" : (parentId + "-")) + this.title.toLowerCase()
     }
-    render(){
-        throw new Error("NOT IMPLEMENTD")
+
+    protected renderWrapper(): HTMLElement {
+        const div = document.createElement('div');
+        div.className = this.className
+        return div
     }
 }
 
-class ContainerElement extends BaseElement {
-    children: BaseElement[]
-    constructor(title: string, children: BaseElement[]){
+class Container extends BaseElement {
+    protected children: BaseElement[]
+    constructor(title: string, children: BaseElement[]) {
         super(title)
         this.children = children
     }
-    render(){
 
+    add(e: BaseElement) {
+        this.children.push(e)
+    }
+
+    override render(parentId?: string) {
+        const div = this.renderWrapper()
+        div.className = "container"
+        const title = document.createElement("p")
+        title.textContent = this.title
+        const items = document.createElement("div")
+        items.className = this.className + " items"
+        this.children.forEach((c) => items.appendChild(c.render(parentId)))
+        div.appendChild(title)
+        div.appendChild(items)
+
+        return div
     }
 }
-class InputElement extends BaseElement {
-    constructor(title: string){
+class Row extends Container {
+    className = "row"
+}
+
+class Column extends Container {
+    className = "column"
+}
+
+class Button extends BaseElement {
+    className = "button"
+    callback: (e: MouseEvent) => void
+
+    constructor(title: string, callback: (e: MouseEvent) => void) {
         super(title)
+        this.callback = callback
+    }
+
+    override render(parentId?: string) {
+        const div = this.renderWrapper()
+        const btn = document.createElement("button")
+        btn.textContent = this.title
+        btn.addEventListener("click", this.callback)
+        div.appendChild(btn)
+        return div
     }
 }
 
-class Row extends ContainerElement {
-}
+// TODO: i am not sure about it
+const injectStyles = (css: string) => {
+    const style = document.createElement('style');
+    style.innerHTML = css;
+    document.head.appendChild(style);
+};
 
-// type UIElement = {
-//     type: UIElementType
-//     title: string
+injectStyles(`
+.column {
+    display: flex;
+    flex-direction: column;
+    margin: 5px;
+}
+.row {
+    display: flex;
+    flex-direction: row;
+    margin: 5px;
+}
+.container {
+    display: flex;
+    flex-direction: column;
+    margin: 5px;
+}
+.items {
+    border: 1px solid rgb(54, 54, 54);
+}
+`);
+
+
+export { BaseElement, Button, Column, Row }
+
+
+// .container {
+//     display: flex;
+//     flex-direction: column;
+//     /* margin-left: 20px; */
+//     margin: 10px;
 // }
 
+// /* .container button {
+//     color: brown;
+// } */
 
+// .button {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+//     /* margin-left: 20px; */
+//     margin: 10px;
+// }
 
-const qparametersLayout = {
-    "type": "container",
-    "title": "Parameters",
-    "items": [
-        {
-            "type": "container",
-            "title": "Navigation",
-            "items": [
-                // {
-                //     "type": "label",
-                //     "title": `Image ${currentImage}/${loadedImages.length}`
-                // },
-                {
-                    "type": "button",
-                    "title": "Prev",
-                    "action": () => {
-                        currentImage = Math.max(0, currentImage - 1)
-                        render()
-                    }
-                },
-                {
-                    "type": "button",
-                    "title": "Next",
-                    "action": () => {
-                        currentImage = Math.min(loadedImages.length, currentImage + 1)
-                        render()
-                    }
-                },
-            ]
-        },
-        {
-            "type": "container",
-            "title": "AAA",
-            "items": [
-                {
-                    "type": "button",
-                    "title": "Square",
-                    "action": () => {
-                        state = State.SQARE
-                    }
-                }
-            ]
-        },
-        {
-            "type": "button",
-            "title": "Load files",
-            "action": loadFiles
-        },
-    ]
-}
+// .button button {
+//     width: fit-content;
+// }
+
+// .label {
+//     display: flex;
+//     flex-direction: row;
+//     align-items: center;
+//     /* margin-left: 20px; */
+//     margin: 10px;
+//     border: 4px black;
+//     height: 30px;
+//     gap: 10px;
+// }
+
+// .input {
+//     display: flex;
+//     flex-direction: row;
+//     align-items: center;
+//     /* margin-left: 20px; */
+//     margin: 10px;
+//     border: 4px black;
+//     height: 30px;
+//     gap: 10px;
+// }
+
+// .input input {
+//     width: 150px;
+// }
+
+// .textarea {
+//     display: flex;
+//     flex-direction: column;
+//     margin: 10px;
+//     border: 4px black;
+// }
+
+// .textarea textarea{
+//     resize: none;
+// }
+// .items {
+//     border: 1px solid rgb(54, 54, 54);
+// }
+
+// /* .column{
+//     margin: 10px;
+//     display: flex;
+//     flex-direction: column;
+// }
+
+// .row{
+//     height: 30px;
+//     display: flex;
+//     flex-direction: row;
+//     gap: 10px;
+// } */
